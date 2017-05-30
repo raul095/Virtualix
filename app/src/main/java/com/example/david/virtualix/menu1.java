75f61599ad667972b68bf29e4b79ec9467dde684
raul095 <raulmorianocompra@gmail.com>
2017-05-30 22:22:57 +0200

    30/05 22:22

--- app/src/main/java/com/example/david/virtualix/menu1.java
+++ app/src/main/java/com/example/david/virtualix/menu1.java
@@ -5,13 +5,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 
 public class menu1 extends Fragment {
+
+    private WebView wv;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.miscursos, container, false);
+        View V = inflater.inflate(R.layout.miscursos, container, false);
+
+        wv = (WebView) V.findViewById(R.id.mcwebview);
+        wv.loadUrl("file:///android_asset/www/principal.html");
+        return V;
     }
 }
