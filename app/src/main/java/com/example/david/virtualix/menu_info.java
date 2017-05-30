3991ecb6abd31455c15f8ab379ec1d0b77e851d2
raul095 <raulmorianocompra@gmail.com>
2017-05-30 16:19:32 +0200

    30/05 16:19

--- app/src/main/java/com/example/david/virtualix/menu_info.java
+++ app/src/main/java/com/example/david/virtualix/menu_info.java
@@ -5,16 +5,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewPropertyAnimator;
+import android.webkit.WebView;
 
 /**
  * Created by David on 29/05/2017.
  */
 
 public class menu_info extends Fragment {
+
+    private WebView wv;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.quienes_somos, container, false);
+        View V = inflater.inflate(R.layout.info_fp, container, false);
+
+        wv = (WebView) V.findViewById(R.id.webview);
+        wv.loadUrl("file:///android_asset/www/info1.html");
+        return V;
     }
 }
--- app/src/main/java/com/example/david/virtualix/menu_info2.java
+++ app/src/main/java/com/example/david/virtualix/menu_info2.java
@@ -10,11 +10,11 @@ import android.view.ViewGroup;
  * Created by David on 29/05/2017.
  */
 
-public class menu_share extends Fragment {
+public class menu_info2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.compartir, container, false);
+        return inflater.inflate(R.layout.quienes_somos, container, false);
     }
 }
