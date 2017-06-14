d01280f73768d4dce613a94a16c67069e031f1c0
raul095 <raulmorianocompra@gmail.com>
2017-06-14 20:45:06 +0200

    14/06 20:44

--- app/src/main/java/com/example/david/virtualix/menu4.java
+++ app/src/main/java/com/example/david/virtualix/menu4.java
@@ -30,7 +30,6 @@ public class menu4 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.calculadora, container, false);
 
         // INICIALIZACION DE OBJETOS
